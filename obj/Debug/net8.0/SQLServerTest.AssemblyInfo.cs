a04c89fbe4c44524bd27821c3e4dc46f25df0ddd
James Campbell (Kings Academy Binfield) <J.Campbell@kingsacademies.uk>
2024-06-06 15:13:44 +0100

    test

--- obj/Debug/net8.0/SQLServerTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/SQLServerTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLServerTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdce87afbd4ca85614f7cb25bca6dbd8973c728c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLServerTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLServerTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
